d77d539e7e71ce2a23fb5caf1125c4ee17596e94
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-06-17 22:18:55 +0800

    0617

---
+++ crashk_crashlytics/src/main/java/com/mozhimen/crashk/crashlytics/CrashKCrashlyticsMgr.kt
@@ -0,0 +1,18 @@
+package com.mozhimen.crashk.crashlytics
+
+import com.mozhimen.libk.google.firebase.basic.optins.OPlugin_ClassPath_FirebaseCrashlytics
+
+/**
+ * @ClassName FirebaseCrashlytics
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2024/6/17
+ * @Version 1.0
+ */
+object CrashKCrashlyticsMgr {
+    @JvmStatic
+    @OPlugin_ClassPath_FirebaseCrashlytics
+    fun init() {
+
+    }
+}
